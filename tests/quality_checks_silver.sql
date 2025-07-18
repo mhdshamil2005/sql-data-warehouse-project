6eb45dc9a277909ee3444b3bfb7c37a4185af769
mhdshamil2005 <muhammadshamil005@gmail.com>
2025-07-18 19:08:38 +0530

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -16,6 +16,196 @@ Usage Notes:
     - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
+-- ====================================================================
+-- Checking Bronze Layer
+-- ====================================================================
+
+--- CHECK Bronze.crm_cust_info ---	  
+select
+cst_id,
+COUNT(*)
+from Bronze.crm_cust_info
+group by cst_id
+having COUNT(*) > 1 or cst_id is NULL
+
+select cst_key
+from Bronze.crm_cust_info
+where trim (cst_key) != cst_key
+
+select cst_firstname
+from Bronze.crm_cust_info
+where TRIM (cst_firstname) != cst_firstname
+
+select cst_lastname
+from Bronze.crm_cust_info
+where TRIM (cst_lastname) != cst_lastname
+
+select distinct cst_marital_status
+from Bronze.crm_cust_info
+
+select distinct cst_gndr
+from Bronze.crm_cust_info
+
+
+--- CHECK Bronze.crm_prd_info ---	
+select
+prd_id,
+count(*)
+from Bronze.crm_prd_info
+group by prd_id
+having count(*) > 1 
+
+select prd_nm
+from Bronze.crm_prd_info
+where trim(prd_nm) != prd_nm
+
+select prd_cost
+from Bronze.crm_prd_info
+where prd_cost < 0 or prd_cost is null
+
+select distinct prd_line
+from Silver.crm_prd_info
+
+select*
+from Bronze.crm_prd_info
+where prd_end_dt < prd_start_dt
+
+select*,
+LEAD (prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) as prd_end_dt
+from Bronze.crm_prd_info
+
+
+--- CHECK Bronze.crm_sales_details ---
+select *,sls_ord_num
+from bronze.crm_sales_details
+where trim(sls_ord_num) != sls_ord_num
+
+SELECT sls_prd_key
+FROM Bronze.crm_sales_details
+where sls_prd_key NOT IN (select distinct prd_key from Silver.crm_prd_info)
+
+SELECT sls_cust_id
+FROM Bronze.crm_sales_details
+where sls_cust_id not IN (select distinct cst_id from Silver.crm_cust_info)
+
+select
+nullif(sls_order_dt,0)
+from Bronze.crm_sales_details
+where sls_order_dt <= 0
+OR LEN(sls_order_dt) != 8
+OR sls_order_dt > 20500101
+OR sls_order_dt < 19000101
+
+select
+sls_ship_dt
+from Bronze.crm_sales_details
+where sls_ship_dt <= 0
+OR LEN(sls_ship_dt) != 8
+OR sls_ship_dt > 20500101
+OR sls_ship_dt < 19000101
+
+select
+sls_due_dt
+from Bronze.crm_sales_details
+where sls_due_dt <= 0
+OR LEN(sls_due_dt) != 8
+OR sls_due_dt > 20500101
+OR sls_due_dt < 19000101
+
+select* from Bronze.crm_sales_details
+where sls_order_dt > sls_ship_dt or sls_order_dt > sls_due_dt
+
+select 
+sls_sales,
+sls_quantity,
+sls_price,
+CASE WHEN sls_sales is null or sls_sales <=0 or sls_sales != sls_quantity * ABS(sls_price)
+		THEN sls_quantity * ABS(sls_price)
+	ELSE sls_sales
+END,
+sls_quantity,
+CASE WHEN sls_price is null or sls_price <= 0
+		THEN ABS(sls_sales)/nullif(sls_quantity,0)
+	ELSE sls_price
+END
+from silver.crm_sales_details
+where sls_sales != sls_quantity * sls_price 
+or sls_sales is null or sls_quantity is null or sls_price is null
+or sls_sales <=0 or sls_quantity <=0 or sls_price <=0
+order by sls_sales
+
+
+--- CHECK Bronze.erp_cust_az12 ---
+
+select
+cid,
+CASE WHEN cid LIKE 'NAS%' THEN REPLACE (cid,'NAS','')
+	 ELSE cid
+END
+from bronze.erp_cust_az12
+WHERE CASE WHEN cid LIKE 'NAS%' THEN REPLACE (cid,'NAS','')
+	 ELSE cid end
+	 not in (
+select distinct cst_key from Bronze.crm_cust_info)
+
+select bdate
+from Bronze.erp_cust_az12
+where bdate > GETDATE()
+
+select gen
+from Bronze.erp_cust_az12
+
+
+--- CHECK Bronze.erp_loc_a101 ---
+select distinct cid
+from Bronze.erp_loc_a101
+
+select
+CASE WHEN cid like 'AW-%' THEN REPLACE(cid,'-','')
+	 ELSE cid
+END cid
+from Bronze.erp_loc_a101
+where CASE WHEN cid like 'AW-%' THEN REPLACE(cid,'-','')
+	 ELSE cid end
+	  not in (
+select cst_key from bronze.crm_cust_info)
+
+SELECT DISTINCT
+cntry,
+CASE WHEN UPPER(TRIM(cntry)) = 'DE' THEN 'Germany'
+	 WHEN UPPER(TRIM(cntry)) IN ('US','USA') THEN 'United States'
+	 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+	 ELSE TRIM(cntry) 
+END cntry
+from Bronze.erp_loc_a101
+
+select distinct cntry
+from silver.erp_loc_a101
+
+
+--- CHECK Bronze.erp_px_cat_g1v2 ---
+select id
+from Bronze.erp_px_cat_g1v2
+where id not in (
+select cat_id from Silver.crm_prd_info)
+
+select distinct
+cat
+from Bronze.erp_px_cat_g1v2
+
+select distinct
+subcat
+from Bronze.erp_px_cat_g1v2
+
+select distinct
+maintenance
+from Bronze.erp_px_cat_g1v2
+
+select
+maintenance
+from Bronze.erp_px_cat_g1v2
+where maintenance != trim(maintenance)
+
 
 -- ====================================================================
 -- Checking 'silver.crm_cust_info'
